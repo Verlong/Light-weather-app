a902e046ef2efdcde2dd25d6f387523cb2c3dd97
Verlong <bonko23@gmail.com>
2022-07-16 16:41:20 +0100

    to comment all "F", correct size of vidjet weather

--- src/app.js
+++ src/app.js
@@ -116,32 +116,30 @@ function handleSubmit(event) {
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
+//function displayFahrenheitTemperature(event) {
+ // event.preventDefault();
+ // let temperatureElement = document.querySelector("#temperature");
   //remove .active fron "C" to "F"
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
+ // celsiusLink.classList.remove("active");
+ // fahrenheitLink.classList.add("active");
+ // let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+ // temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+//}
 
-function displayCelsiusTemperature(event) {
-  event.preventDefault();
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+//function displayCelsiusTemperature(event) {
+//  event.preventDefault();
+//  celsiusLink.classList.add("active");
+//  fahrenheitLink.classList.remove("active");
+//  let temperatureElement = document.querySelector("#temperature");
+//  temperatureElement.innerHTML = Math.round(celsiusTemperature);}
 
-}
+//let celsiusTemperature = null;
 
-let celsiusTemperature = null;
+//let fahrenheitLink = document.querySelector("#fahrenheit-link");
+//fahrenheitLink.addEventListener("click", //displayFahrenheitTemperature);
 
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature)
+//let //celsiusLink = document.querySelector("#celsius-link");
+//celsiusLink.addEventListener("click", displayCelsiusTemperature)
 search("Ivano-Frankivsk");
 
 
